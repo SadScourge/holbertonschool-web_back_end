1d730aa0fe946482c5a1c801e3cf211a9b52461c
SadScourge <darklzar@gmail.com>
2024-12-11 14:15:08 +0100

    added comment and typing to my function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains an asynchronous generator function.
+"""
+
 import random
 import asyncio
+from typing import AsyncGenerator
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float]:
+    """
+    Asynchronous generator that yields a random float between 0 and 10.
+
+    The generator will yield 10 random numbers, each after a 1-second delay.
+    """
     for index in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
