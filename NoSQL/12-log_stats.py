dd4986eeb2b27187e38a921b4637a26d93c7edff
SadScourge <darklzar@gmail.com>
2024-12-13 19:36:07 +0100

    corrected some requierements

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -17,8 +17,8 @@ def log_stats():
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    stCheck = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{stCheck} status check")
 
 
 if __name__ == "__main__":
