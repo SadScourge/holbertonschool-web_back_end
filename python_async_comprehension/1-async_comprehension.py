39d720762a1bbe804ce534e298e8cc059b5b89e7
SadScourge <darklzar@gmail.com>
2024-12-13 21:23:38 +0100

    corrected typing

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """
 This module contains a coroutine that collects 10 random numbers using an async generator.
 """
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> typing.List[float]:
     """
     Asynchronously collect 10 random numbers using an async generator
     """
