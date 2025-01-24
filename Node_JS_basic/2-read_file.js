343014f0b858e309a76b0b9d2f8fc14d68c15f69
SadScourge <darklzar@gmail.com>
2025-01-24 23:49:40 +0100

    corrected linting

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,33 +1,32 @@
 const fs = require('node:fs');
 
 function countStudents(path) {
-    try {
-        data = fs.readFileSync(path, "utf-8")
+  try {
+    data = fs.readFileSync(path, 'utf-8');
 
-        const linearray = data.trim().split('\n').filter((line) => line.trim() !== '');
-        number = linearray.slice(1);
-        const fields = {};
+    const linearray = data.trim().split('\n').filter((line) => line.trim() !== '');
+    number = linearray.slice(1);
+    const fields = {};
 
-        number.forEach((line) => {
-            const split = line.split(',');
-            if (split.length < 4) {
-                return
-            };
-            const name = split[0].trim();
-            const field = split[3].trim();
-            if (!fields[field]) {
-                fields[field] = [];
-            }
-            fields[field].push(name);
-        });
-        console.log(`Number of students: ${number.length}`)
-        for (const [field, name] of Object.entries(fields)) {
-            console.log(`Number of students in ${field}: ${name.length}. List: ${name.join(', ')}`);
-          }
+    number.forEach((line) => {
+      const split = line.split(',');
+      if (split.length < 4) {
+        return;
+      }
+      const name = split[0].trim();
+      const field = split[3].trim();
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(name);
+    });
+    console.log(`Number of students: ${number.length}`);
+    for (const [field, name] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${name.length}. List: ${name.join(', ')}`);
     }
-    catch (error) {
-        throw new Error("Cannot load the database")
-    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
